976ddb33a5e4c6d13d15c434145d083f2368b191
Kenichi <kenichixie@gmail.com>
2018-05-03 09:51:44 +0700

    finish auth

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const CookieSession = require('cookie-session');
+const passport = require('passport');
+const { mongoUri, cookieKey } = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(mongoUri);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ h1: 'there' });
-});
+app.use(
+  CookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+router.get('/google/callback', passport.authenticate('google'));
+
+router.get('/logout',(req, res) => {
+  req.logout();
+  res.send(req.user);
+})
+
+router.get('/getme', (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
